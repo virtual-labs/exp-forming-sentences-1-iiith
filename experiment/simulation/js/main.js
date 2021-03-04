7a7c7104e3f99b15cd6750d0d9ffcb8ef6645a0c
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2021-03-04 15:06:26 +0530

    Updated js function

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,12 @@
-//Your JavaScript goes in here
+<script type="text/javascript">
+
+function getOption(lang){
+	if(lang=="null")
+	{
+		alert("Select language");
+		return;
+	}
+	$('#words_sentence').load('CLexp1.php?lang='+lang+'&words_selected=%&words=%&possible_sent=%&turn=0&position=%');
+}
+
+</script>
